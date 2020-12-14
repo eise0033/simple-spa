9af93c19899fa088efad6c551351c94ce86aa5be
Nicki <eise0033@algonquinlive.com>
2020-12-14 14:47:29 -0500

    Finished the actors section

--- assign3-spa-f2020/js/main.js
+++ assign3-spa-f2020/js/main.js
@@ -1,12 +1,11 @@
 //app is for general control over the application
 //and connections between the other components
-
-let baseURL = 'https://api.themoviedb.org/3/'
-let configData= null;
-let searchURL = null;
-let api_key = '23c75e09b823ce4864be9c6b9a262615'
-let df = document.createDocumentFragment();
 const APP = {
+  baseURL: 'https://api.themoviedb.org/3/',
+  configData: null,
+  searchURL: null,
+  api_key: '23c75e09b823ce4864be9c6b9a262615',
+  df: document.createDocumentFragment(),
   init: () => {
     //this function runs when the page loads
     SEARCH;
@@ -16,36 +15,52 @@ const APP = {
 //search is for anything to do with the fetch api
 const SEARCH = {
   results: ["Scarlett Johansson", "Brad Pitt", "Dwayne Johnson", "Tom Hanks", "Will Smith", "Johnny Depp", "Arnold Schwarzenegger"],
-findActors: (ev) => {
-ev.preventDefault();
-let txt = document.getElementById('search').value.trim();
+  findActors: (ev) => {
+    ev.preventDefault();
+    let txt = document.getElementById('search').value.trim();
 //user has filled in search word
-let url = APP.baseURL.concat('&query=', txt);
-fetch(url)
-.then((resp)=>{
-if(resp.ok){
-return resp.json();
-}else{
-throw new Error('Failed to fetch search results');
-}
-});
-.then((data)=>{
+    let url = APP.baseURL.concat('&query=', txt);
+    fetch(url)
+    .then((resp)=>{
+      if(resp.ok){
+        return resp.json();
+      }else{
+        throw new Error('Failed to fetch search results');
+      }
+    })
+    .then((data)=>{
 //data is the object returned from the API
 //save in the SEARCH.results property
-SEARCH.results = data.results;
-ACTORS;
+      SEARCH.results = data.results;
+      ACTORS;
     })
   }
 };
 
 //actors is for changes connected to content in the actors section
 const ACTORS = {
-  cards: 
+  cards: (ev) => {
+    ev.preventDefault();
+    let txt = document.getElementById('actors');
+    let url = APP.baseURL.concat('&query=', txt);
+    fetch(url)
+    .then((resp)=>{
+      if(resp.ok){
+        return resp.json();
+      }else{
+        throw new Error('Failed to fetch actors');
+      }
+    })
+    .then((data)=>{
+      ACTORS.results = data.results;
+      MEDIA;
+    })
+  }
 };
 
 //media is for changes connected to content in the media section
 const MEDIA = {
-
+  
 };
 
 //storage is for working with localstorage
